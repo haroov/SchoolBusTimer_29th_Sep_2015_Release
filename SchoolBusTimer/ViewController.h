f5bfb1a5f9ace1ec664e56ddb9115251c9acc259
Radul Popov <radulpopov116@gmail.com>
2014-10-28 20:43:34 +0200

    Initial Commit

---
+++ SchoolBusTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolBusTimer
+//
+//  Created by Radul Popov on 10/28/14.
+//  Copyright (c) 2014 Radul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
